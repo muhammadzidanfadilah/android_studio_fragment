20cc53e5fe5826ad49e9bf9e760e089a3290143c
muhammadzidanfadillah <115553474+muhammadzidanfadilah@users.noreply.github.com>
2023-12-13 02:20:35 -0800

    Add files via upload

---
+++ src/main/java/com/example/uts/splash.java
@@ -0,0 +1,35 @@
+package com.example.uts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uts.MainActivity;
+
+public class splash extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
